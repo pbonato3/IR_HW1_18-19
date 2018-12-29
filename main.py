c12ec51b7c302d12aeecc8189eb992ca16f67634
Paolo <paolo.bonato.12@gmail.com>
2018-12-29 19:26:52 +0100

    added some statistics in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 # paths of the files containing the evaluations
-eval_files_paths = ["./0/run_0_eval.txt", "./1/run_1_eval.txt", "./2/run_2_eval.txt", "./3/run_3_eval.txt"]
+eval_files_paths = ["./0/eval_0.txt", "./1/eval_1.txt", "./2/eval_2.txt", "./3/eval_3.txt"]
 
 # lists of lists:   [["eval_0_metrics"], ["eval_1_metrics"], ["eval_2_metrics"], ["eval_3_metrics"]]
 MAP = []
@@ -65,19 +65,88 @@ last_index = len(MAP[0]) - 1
 #plt.title("P@10 values per run")
 #plt.show()
 
-
 #plt.plot(['Run_0', 'Run_1', 'Run_2', 'Run_3'],[RPrec[0][last_index], RPrec[1][last_index], RPrec[2][last_index], RPrec[3][last_index]])
 #plt.ylabel('RPrec')
 #plt.title("RPrec values per run")
 #plt.show()
 
 plt.boxplot([MAP[0][:last_index-1], MAP[1][:last_index-1], MAP[2][:last_index-1], MAP[3][:last_index-1]])
-plt.ylabel('MAP')
+plt.title('Boxplot of Average Precision')
+plt.ylabel('AP')
 plt.xlabel('Run_#')
 plt.show()
 
+plt.boxplot([P_10[0][:last_index-1], P_10[1][:last_index-1], P_10[2][:last_index-1], P_10[3][:last_index-1]])
+plt.title('Boxplot of P@10')
+plt.ylabel('P_10')
+plt.xlabel('Run_#')
+plt.show()
+
+plt.boxplot([RPrec[0][:last_index-1], RPrec[1][:last_index-1], RPrec[2][:last_index-1], RPrec[3][:last_index-1]])
+plt.title('Boxplot of RPrec')
+plt.ylabel('RPrec')
+plt.xlabel('Run_#')
+plt.show()
+
+
+print "ANOVA 1-way, AP, all runs:" 
 fvalue, pvalue = stats.f_oneway(MAP[0][:last_index-1], MAP[1][:last_index-1], MAP[2][:last_index-1], MAP[3][:last_index-1])
+print "F_value: " , fvalue , " P_value: ", pvalue
+
+print "ANOVA 1-way, AP, BM25 runs:"  
+fvalue, pvalue = stats.f_oneway(MAP[0][:last_index-1], MAP[2][:last_index-1])
+print "F_value: " , fvalue , " P_value: ", pvalue
+
+print "ANOVA 1-way, AP, TF_IDF runs:"  
+fvalue, pvalue = stats.f_oneway(MAP[1][:last_index-1], MAP[3][:last_index-1])
+print "F_value: " , fvalue , " P_value: ", pvalue
+
+print '\n'
+
+print "ANOVA 1-way, P@10, all runs:" 
+fvalue, pvalue = stats.f_oneway(P_10[0][:last_index-1], P_10[1][:last_index-1], P_10[2][:last_index-1], P_10[3][:last_index-1])
+print "F_value: " , fvalue , " P_value: ", pvalue
+
+print "ANOVA 1-way, P@10, BM25 runs:"  
+fvalue, pvalue = stats.f_oneway(P_10[0][:last_index-1], P_10[2][:last_index-1])
+print "F_value: " , fvalue , " P_value: ", pvalue
+
+print "ANOVA 1-way, P@10, TF_IDF runs:"  
+fvalue, pvalue = stats.f_oneway(P_10[1][:last_index-1], P_10[3][:last_index-1])
+print "F_value: " , fvalue , " P_value: ", pvalue
+
+
+print '\n'
+
+print "ANOVA 1-way, RPrec, all runs:" 
+fvalue, pvalue = stats.f_oneway(RPrec[0][:last_index-1], RPrec[1][:last_index-1], RPrec[2][:last_index-1], RPrec[3][:last_index-1])
+print "F_value: " , fvalue , " P_value: ", pvalue
+
+print "ANOVA 1-way, RPrec, BM25 runs:"  
+fvalue, pvalue = stats.f_oneway(RPrec[0][:last_index-1], RPrec[2][:last_index-1])
+print "F_value: " , fvalue , " P_value: ", pvalue
+
+print "ANOVA 1-way, RPrec, TF_IDF runs:"  
+fvalue, pvalue = stats.f_oneway(RPrec[1][:last_index-1], RPrec[3][:last_index-1])
 print "F_value: " , fvalue , " P_value: ", pvalue
 
 
 
+
+
+########################################################
+# Retrieval with Query Expansion
+########################################################
+
+# paths of the files containing the evaluations
+eval_files_paths = ["./0/eval_0_qe.txt", "./1/eval_1_qe.txt", "./2/eval_2_qe.txt", "./3/eval_3_qe.txt"]
+
+# lists of lists:   [["eval_0_metrics"], ["eval_1_metrics"], ["eval_2_metrics"], ["eval_3_metrics"]]
+MAP = []
+P_10 = []
+RPrec = []
+
+
+# load measures
+load_eval(MAP = MAP, P_10 = P_10, RPrec = RPrec, files_path= eval_files_paths)
+last_index = len(MAP[0]) - 1
